f9cdb3b7f5dda7c99f2b7c206e0230b321c44450
Munia Laun <munialaun@Munias-MacBook-Air.local>
2017-05-24 09:42:15 +0200

    updated

--- app/gui-model/guimodel.ts
+++ app/gui-model/guimodel.ts
@@ -2,7 +2,7 @@ export class GuiModel {
 
     private _guiModel = {
         "application": {
-            "title": "Requirements Engineering Friend Tracker",
+            "title": "Munias Friend Tracker",
             "formList": [
                 {
                     "id": "FriendForm",
@@ -22,6 +22,13 @@ export class GuiModel {
                             "width": 1,
                             "required": true
                         },
+			{			"id": "group",			"type": "autocomplete",			"name": "Group",			"data": [ "Study", "Family", "School" ], 
+			"form": "GroupForm",			"width": 2			},
+			{			"id": "nickname", 
+			"type": "text", 
+			"name": "Nickname", 
+			"width": 2, 
+			"required": true			},
                         {
                             "id":   "location",
                             "type": "autocomplete",
@@ -58,33 +65,98 @@ export class GuiModel {
                         }
                     ]
                 },
-                {
-                    "id": "LocationForm",
-                    "title": "Location",
-                    "formFieldList": [
+		{		"id": "GroupForm", 
+		"title": "Group", 	
+		"formFieldList": [			{			"id": "name",			"type": "text",			"name": "GroupName", 
+			"width": 2,			"required": true			}, 
+ {
+                            "id": "creationDate",
+                            "type": "date",
+                            "name": "CreationDate",
+                            "width": 2
+                        },						
                         {
-                            "id": "name",
+                            "id": "comment",
                             "type": "text",
-                            "name": "LocationName",
+                            "name": "Comments",
                             "width": 2,
-                            "required": true
+                            "height": 4,
+                            "maxLength": 5000,
+                        },
+			{			"name": "Delete" 
+			},			{			"type": "cancelButton", 
+			"name": "Cancel",
+			"type": "deleteButton",			}, 
+			{			"name": "Ok" 
+			}		] 
+},
+                {
+					"id": "ActivityForm",
+					"title": "Activity",
+					"formFieldList": [ 
+						{ 
+							"id": "name",
+							"type": "text",
+							"name": 'ActivityName',
+							"width": 2,
+							"required": true 
+						},
+                        {
+                            "id":   "location",
+                            "type": "autocomplete",
+                            "name": "Location",
+                            "data": [ "Winterthur", "Zürich" ],
+                            "form": "LocationForm",
+                            "width": 2
                         },
                         {
-                            "type": "deleteButton",
-                            "name": "Delete"
-                        },
-                        {
-                            "type": "cancelButton",
-                            "name": "Cancel"
-                        },
-                        {
-                            "type": "okButton",
-                            "name": "Ok"
-                        }
-                    ]
-                }
-            ],
-            "pageList": [
+                            "id": "creationDate",
+                            "type": "date",
+                            "name": "CreationDate",
+                            "width": 2
+                        },						
+						{
+							"type": "deleteButton",
+							"name": "Delete"
+						},
+						{
+							"type": "cancelButton",
+							"name": "Cancel" 
+						},
+						{
+							"type": "okButton",
+							"name": "Ok" 
+						}
+					]
+				},					
+				{
+					"id": "ActivitiesForm",
+					"title": "Activity",
+					"formFieldList": [ 
+						{
+							"id": "activities",
+							"type": "autocomplete",
+							"name": "Activity",
+							"data": ["Something", "Movie Why Him", "Eating Pizza", "Running Eschenberg"],
+							"form": "ActivityForm",
+							"width": 2 
+						},						
+						{
+							"type": "deleteButton",
+							"name": "Delete"
+						},
+						{
+							"type": "cancelButton",
+							"name": "Cancel" 
+						},
+						{
+							"type": "okButton",
+							"name": "Ok" 
+						}
+					]
+				}				
+				
+            ],            "pageList": [
                 {
                     "id": "mainmenu",
                     "name": "MainMenu",
@@ -93,19 +165,33 @@ export class GuiModel {
                             "type": "button",
                             "name": "Friends",
                             "icon": "fa-user",
-                            "color": "blue",
+                            "color": "pink",
                             "page": "friendspage",
                         },
                         {
                             "type": "button",
                             "name": "Location",
-                            "icon": "fa-cubes",
-                            "color": "yellow",
+                            "icon": "fa-location-arrow",
+                            "color": "magenta",
                             "page": "locationspage",
                         },
+
+			{			"type": "button", 
+			"name": "Groups", 
+			"icon": "fa-group", 
+			"color": "wisteria", 
+			"page": "groupspage",			},
+{ 
+							"type": "button",
+							"name": "Activities",
+							"icon": "fa-calendar",
+							"color": "amethyst",
+							"page": "activitiespage", 
+						}	
                     ]
                 },
-                {
+
+			{
                     "id": "friendspage",
                     "elementList": [
                         {
@@ -126,10 +212,8 @@ export class GuiModel {
                             "color": "blue",
                             "search": true,
                             "data": [ { name: "Anton Amacker" }, { name: "Britta Beavers"} ],
-                            "form": {
-                                "form": "FriendForm"
-                            }
-                        },
+                            "page": "friendpage",
+                        }
                     ]
                 },
                 {
@@ -156,9 +240,124 @@ export class GuiModel {
                             "form": {
                                 "form": "LocationForm"
                             }
-                        },
+                        }
                     ]
-                }
+                },
+				{
+					"id": "groupspage",
+					"elementList": [ 
+						{ 
+							"type": "backbutton", 
+						},
+						{ 
+							"type": "newButton",
+							"name": "NewGroup",
+							"icon": "fa-weixin",
+							"color": "green",
+							"form": { 
+								"form": "GroupForm" 
+							}
+						},
+						{
+							"type": "list",
+							"icon": "fa-weixin",
+							"color": "wisteria",
+							"search": true,
+							"data": [ { name: "Study" }, { name: "Family" }, { name: "School"} ],
+							"form": {
+								"form": "GroupForm"
+							}
+						},
+					]
+				},
+				{
+					"id": "friendpage",
+					"elementList": [ 
+						{ 
+							"type": "backbutton", 
+						},
+						{ 
+							"type": "newButton",
+							"name": "EditFriend",
+							"icon": "fa-user",
+							"color": "green",
+							"form": { 
+								"form": "FriendForm" 
+							}
+						},
+						{ 
+							"type": "newButton",
+							"name": "AddActivity",
+							"icon": "fa-calendar",
+							"color": "green",
+							"form": { 
+								"form": "ActivitiesForm" 
+							}
+						},						
+						{
+							"type": "list",
+							"icon": "fa-calendar",
+							"color": "amethyst",
+							"search": true,
+							"data": [ { name: "Movie Why Him" }, { name: "Eating Pizza" }, { name: "Running Eschenberg"} ],
+							"form": {
+								"form": "ActivitiesForm"
+							}
+						}
+					]
+				},
+				{
+					"id": "activitiespage",
+					"elementList": [ 
+						{ 
+							"type": "backbutton", 
+						},
+						{ 
+							"type": "newButton",
+							"name": "New Activity",
+							"icon": "fa-calendar",
+							"color": "green",
+							"form": { 
+								"form": "ActivityForm" 
+							}
+						},
+						{
+							"type": "list",
+							"icon": "fa-calendar",
+							"color": "pumpkin",
+							"search": true,
+							"data": [ { name: "Something" }, { name: "Movie Why Him" }, { name: "Eating Pizza" }, { name: "Running Eschenberg"} ],
+							"page": "activitypage"
+						},
+					]
+				},					
+				{
+					"id": "activitypage",
+					"elementList": [ 
+						{ 
+							"type": "backbutton", 
+						},
+						{ 
+							"type": "newButton",
+							"name": "EditActivity",
+							"icon": "fa-calendar",
+							"color": "green",
+							"form": { 
+								"form": "ActivityForm" 
+							}
+						},
+						{
+							"type": "list",
+							"icon": "fa-user",
+							"color": "blue",
+							"search": true,
+							"data": [ { name: "Anton Amacker" }, { name: "Britta Beavers"} ],
+							"form": {
+								"form": "FriendForm"
+							}
+						}
+					]
+				}				
             ]
         }
     };
